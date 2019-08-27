62dd0a10981d882e54331dfbcafb5a414eea29e6
koszonetdoktor <halmi.gergo@gmail.com>
2019-08-27 20:14:30 +0200

    read temperature

--- temperature/main.go
+++ temperature/main.go
@@ -4,7 +4,27 @@ import (
   "fmt"
   "io/ioutil"
   "strings"
+  "errors"
+  "strconv"
+  "time"
 )
+
+func main() {
+    sensors, err := getSensors()
+    if err != nil {
+        fmt.Println("ERROR", err)
+        return
+    }
+    fmt.Println("SENSORS", sensors)
+    for {
+      temperature, err := readTemperature(sensors[0])
+      if err != nil {
+          fmt.Println(err)
+      }
+      fmt.Println("Temp: ", temperature)
+      time.Sleep(1 * time.Second)
+    }
+}
 
 func getSensors() ([]string, error) {
   data, err := ioutil.ReadFile("/sys/bus/w1/devices/w1_bus_master1/w1_master_slaves")
@@ -22,12 +42,33 @@ func getSensors() ([]string, error) {
   return sensors, nil
 }
 
-func main() {
-    sensors, err := getSensors()
-    if err != nil {
-        fmt.Println("ERROR", err)
-        return
-    }
-    fmt.Println("SENSORS", sensors)
+func readTemperature(sensor string) (float64, error) {
+  data, err := ioutil.ReadFile("/sys/bus/w1/devices/" + sensor + "/w1_slave")
+  if err != nil {
+      return 0.0, errors.New("Sensor read error")
+  }
+  
+  raw := string(data)
+  
+  i := strings.LastIndex(raw, "t=")
+  
+  if i == -1 {
+      return 0.0, errors.New("Sensor read error")
+  }
+  
+  c, err := strconv.ParseFloat(raw[i+2:len(raw)-1], 64)
+  if err != nil {
+      return 0.0, errors.New("Sensor read error")
+  }
+  
+  return c/ 1000.0, nil
 }
+
+
+
+
+
+
+
+
 
